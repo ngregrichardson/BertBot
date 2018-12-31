1b96d0b9d46a43922a3709783a03564bcc11043e
Glitch (discreet-quokka) <none>
2018-12-31 17:19:11 +0000

    🗃🏭 Checkpoint ./ui/index.html:1671621/93 ./server.js:1671621/7 ./conf.json:1671621/2 ./index.html:1671621/41

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 app.use(express.static('public'));
 
 app.get('/', function(request, response) {
-  response.sendFile(__dirname + '/ui/index.html');
+  response.sendFile(__dirname + '/index.html');
 });
 
 const listener = app.listen(process.env.PORT, function() {
