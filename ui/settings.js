74b0664535e41125e87764d849fdc23d1d3c0f06
Glitch (discreet-quokka) <none>
2018-12-31 16:57:16 +0000

    💖🌄 Checkpoint ./ui/settings.js:1671621/3173

--- ui/settings.js
+++ ui/settings.js
@@ -1,2 +1,52 @@
 var botName = $("input[name='botName']");
-var team Number
+var teamNumber = $("input[name='teamNumber']");
+var discordServerId = $("input[name='discordServerId']");
+var trelloNotificationsOn = $("input[name='trelloNotificationsOn']");
+var trelloNotifications = $("#trelloNotifications");
+var trelloNotificationChannelId = $("input[name='trelloNotificationChannelId']");
+var trelloPollInterval = $("input[name='trelloPollInterval']");
+var watchedTrelloBoardIds = $("input[name='watchedTrelloBoardIds']");
+var createCard = $("input[name='createCard']");
+var updateCard = $("input[name='updateCard']");
+var deleteCard = $("input[name='deleteCard']");
+var commentCard = $("input[name='commentCard']");
+var addMemberToCard = $("input[name='addMemberToCard']");
+var removeMemberFromCard = $("input[name='removeMemberFromCard']");
+var createList = $("input[name='createList']");
+var updateList = $("input[name='updateList']");
+var addAttachmentToCard = $("input[name='addAttachmentToCard']");
+var deleteAttachmentFromCard =$("input[name='deleteAttachmentFromCard']");
+var addChecklistToCard = $("input[name='addChecklistToCard']");
+var removeChecklistFromCard = $("input[name='removeChecklistFromCard']");
+var updateCheckItemStateOnCard = $("input[name='updateCheckItemStateOnCard']");
+var trelloPrefix = $("input[name='trelloPrefix']");
+var orderRequestEmailSystemOn = $("input[name='orderRequestEmailSystemOn']");
+var orderRequestSystem = $("#orderRequestSystem");
+var orderPlacedChecklistItemName = $("input[name='orderPlacedChecklistItemName']");
+var orderPlacedListName = $("input[name='orderPlacedListName']");
+var orderRequestedListName = $("input[name='orderRequestedListName']");
+var swearFilterOn = $("input[name='swearFilterOn']");
+var swearFilter = $("#swearFilter");
+var swearFilterWhitelistedChannelNames = $("input[name='swearFilterWhitelistedChannelNames']");
+var blaiseWhitelistedChannelNames = $("input[name='blaiseWhitelistedChannelNames']");
+var restrictedCommandRoles = $("input[name='restrictedCommandRoles']");
+
+$(function() {
+  
+});
+
+function toggleTrelloNotifications() {
+  trelloNotifications.toggle();
+}
+
+function toggleOrderRequestSystem() {
+  
+}
+
+function toggleSwearFilter() {
+  
+}
+
+function save() {
+  
+}
