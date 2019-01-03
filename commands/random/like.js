1a41eae45a0d6ef201b30807f56a9e2d89eebdae
Glitch (bert-bot-demo) <none>
2019-01-03 16:44:37 +0000

    🍮🏩 Checkpoint ./commands/random/like.js:1671621/3559

--- commands/random/like.js
+++ commands/random/like.js
@@ -1,6 +1,5 @@
-const DadJokes = require('dadjokes-wrapper');
-const dj = new DadJokes();
 const commando = require('discord.js-commando');
+const Discord = require('discord.js');
 
 /* Config */
 const fs = require('fs');
@@ -12,35 +11,39 @@ class Like extends commando.Command {
       name: 'like',
       group: 'random',
       memberName: 'like',
-      description: 'Tells you how many likes you have',
-      throttling: {
-        usages: 1,
-        duration: 60
-      },
+      description: 'Tells you the amount of likes you have.',
+      // throttling: {
+      //   usages: 1,
+      //   duration: 30
+      // },
       args: [{
         key: 'term',
-        prompt: 'Whether to show your like count or the top likes',
+        prompt: 'Would you like to display the top 5 liked members or your count?',
         type: 'string',
         default: ''
       }]
     });
   }
   async run(message, { term }) {
-    if(config.blaiseWhitelistedChannelNames.includes(message.channel.name) || config.blaiseWhitelistedChannelNames.includes("allowAll")) {
-      if(term) {
-        dj.searchJoke({'term': term}).then(function(res) {
-          if(res) {
-            message.channel.send(res.results[Math.floor(Math.random() * res.results.length)].joke);
-          }else {
-            message.channel.send('Sorry, there are no jokes with that keyword');
-          }
-        });
-      } else {
-        dj.randomJoke().then(function(res) {
-          message.channel.send(res);
-        });
+    fs.readFile('commands/random/likes.json', function(err, response) {
+      if(err) {
+        console.log(err);
       }
-    }
+      var data = JSON.parse(response);
+      console.log(data);
+      if(term == 'top') {
+        for(var thing in data) {
+          console.log(thing);
+        }
+        //let embed = new Discord.RichEmbed().setTimestamp(Date.now()).setColor("#127ABD").setTitle(`**Top Liked Users**`).setDescription(`**1. ** ${}\n\n**Meeting Plans:** ${meetings.meetings[i].description}`);
+      }else if(term == 'count') {
+        if(data[message.author.username]) {
+        message.author.send("You have " + data[message.author.username] + " likes!");
+      }else {
+        message.author.send("You do not have any likes yet.");
+      }
+      }
+    });
   }
 }
 module.exports = Like;
